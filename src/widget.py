e331f67be0aacdfd3703131d129f69015b08d032
TENGIZ TOMADZE <tengiztomadze@gmail.com>
2025-04-16 10:22:44 +0300

    add commit in homeworke 11.1

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,3 @@
-from coverage import parser
-from mypy.errors import MypyError
 from src.masks import get_mask_account, get_mask_card_number
 
 
@@ -14,21 +12,8 @@ def mask_account_card(type_number: str) -> str:
         number = get_mask_card_number(type_number[-16:])
         updated_card_number = " ".join(type_number.split()[:-1]) + " " + number
         return updated_card_number
-    # elif 'MasterCard' in type_number_list or 'Maestro' in type_number_list:
-    #     return f'{type_number_list[0]} {get_mask_card_number(type_number_list[1])}'
-    # elif 'Visa' in type_number_list:
-    #     number_card = []
-    #     simbol_card = []
-    #     for i in type_number_list:
-    #         if i.isdigit():
-    #             number_card.append(i)
-    #         elif i.isalpha():
-    #             simbol_card.append(i)
-    #     total_number_card = ''.join(number_card)
-    #     return f'{simbol_card[0]} {simbol_card[1]} {get_mask_card_number(total_number_card)}'
 
 
-# "2024-03-11T02:26:18.671407"
 def get_date(date_input: str) -> str:
     ''' Функция, которая меняет вормат даты. '''
     return ".".join(date_input[:10].split("-")[::-1])
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,15 +1,13 @@
 import pytest
 
-from src.masks import get_mask_card_number
-
-from src.masks import get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_mask_card_number(card_number):
     assert get_mask_card_number(card_number) == '7000 79** **** 6361'
 
 
-@pytest.mark.parametrize('card',[
+@pytest.mark.parametrize('card', [
     ('700079228960636121'),
     ('7000792289606'),
     ('')
@@ -23,7 +21,7 @@ def test_get_mask_account(card_account):
     assert get_mask_account(card_account) == '**4305'
 
 
-@pytest.mark.parametrize('account',[
+@pytest.mark.parametrize('account', [
     ('73654108430135874305123'),
     ('7365410843013587'),
     ('')
